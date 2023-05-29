fa7d44e371c7d659dd77e55147d520727910e69c
jolin1215 <127169306+jolin1215@users.noreply.github.com>
2023-05-29 20:19:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as pl
 
-# 讀取Excel檔案
 data = pd.read_excel('https://raw.githubusercontent.com/jolin1215/FinalProject/main/活頁簿2.xlsx')
 
-# 定義函式，根據讀書時間分配到對應的區間
 def assign_time_range(hours):
     if hours <= 10:
         return '0~10hr'
@@ -16,18 +14,13 @@ def assign_time_range(hours):
     else:
         return '30~hr'
 
-# 新增讀書時間區間欄位
 data['時間區間'] = data['平均一個星期讀書時間'].apply(assign_time_range)
 
-# 以時間區間和性別分組，計算每個時間區間的人數
 grouped_data = data.groupby(['時間區間', '生理性別']).size().unstack().fillna(0)
 
-# 使用 Streamlit 的plotly_chart方法繪製雙曲線圖
 st.plotly_chart(grouped_data.plot(kind='line').figure)
 
-# 設定圖表標籤等
 pl.xlabel('time of range')
 pl.ylabel('number of people')
 
-# 使用 Streamlit 的.pyplot方法顯示圖表
 st.pyplot(pl)
